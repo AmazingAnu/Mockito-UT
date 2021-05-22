a2ff5de79497b0a64bc1127b849920a81587f167
Anu <Anu@LAPTOP-G1IND42N>
2021-05-22 19:24:49 +0530

    Initial commit

---
+++ src/main/java/com/ut/mockito/UtMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.ut.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ut/mockito/UtMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
